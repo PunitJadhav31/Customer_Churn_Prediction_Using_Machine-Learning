03b9d523c9cd0ba6307e287bc207c99430dd8f86
Punit Vishwas Jadhav <punitvishwasjadhav@Punits-MacBook-Air.local>
2026-05-15 06:08:22 -0500

    Complete model training and prediction pipeline

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 # Gender -> 1 Female 9 Male
 # Chrun -> 1 Yes 0 No
 # Scaler is exported as scaler.pkl
+# Model is exported as model.pkl
+# Order of the X -> 'Age', 'Gender', 'Tenure', 'MonthlyCharges'
